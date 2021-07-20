8a044ffb48ac58e0c8ee1e64c543825ff45eb93d
averichev <averichev@yandex.ru>
2021-07-20 13:31:28 +0300

    test(TwoSum): Изменения в тесте

--- Test/TwoSum/Solution1Test.cs
+++ Test/TwoSum/Solution1Test.cs
@@ -1,3 +1,4 @@
+using System;
 using LeetCode.TwoSum.Solutions._1;
 using Xunit;
 
@@ -10,6 +11,7 @@ namespace Test.TwoSum
         {
             var solution = new Solution();
             var result = solution.TwoSum(new[] {2, 7, 11, 15}, 9);
+            Array.Sort(result);
             Assert.True(result.Length == 2);
             Assert.True(result[0] == 0);
             Assert.True(result[1] == 1);
@@ -20,6 +22,7 @@ namespace Test.TwoSum
         {
             var solution = new Solution();
             var result = solution.TwoSum(new[] {3, 2, 4}, 6);
+            Array.Sort(result);
             Assert.True(result.Length == 2);
             Assert.True(result[0] == 1);
             Assert.True(result[1] == 2);
@@ -30,6 +33,7 @@ namespace Test.TwoSum
         {
             var solution = new Solution();
             var result = solution.TwoSum(new[] {3, 3}, 6);
+            Array.Sort(result);
             Assert.True(result.Length == 2);
             Assert.True(result[0] == 0);
             Assert.True(result[1] == 1);
@@ -40,6 +44,7 @@ namespace Test.TwoSum
         {
             var solution = new Solution();
             var result = solution.TwoSum(new[] {2, 5, 5, 11}, 10);
+            Array.Sort(result);
             Assert.True(result.Length == 2);
             Assert.True(result[0] == 1);
             Assert.True(result[1] == 2);
